d145d61a81395b40a0c30b364c163ea1a2355021
Cesar Bueno <barddes@gmail.com>
2021-04-21 23:15:44 +0000

    fix environment

--- target.cpp
+++ target.cpp
@@ -1,20 +1,6 @@
 #include <iostream>
 #include "png-mutator.h"
 
-void inverte(std::vector<uint8_t>& data) {
-	try {
-		Png png = Png(data);
-		std::cout << "Width:  " << png.width() << std::endl;
-		std::cout << "Height: " << png.height() << std::endl;
-	} catch (...) {
-		return;
-	}
-
-	throw;
-}
-
-
-
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 	std::vector<uint8_t> data(Data, Data+Size);
 
